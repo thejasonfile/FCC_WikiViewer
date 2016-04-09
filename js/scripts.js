398fc9851041a90407d04e331f42d238f7f23bc5
Jason Arnold <jason@thejasonfile.com>
2016-04-09 15:12:00 -0400

    finished autocomplete functionality, started on styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,16 @@
-// $(document).ready(function(){
-
-// 	function wikiSearch() {
-// 		console.log('running search')
-// 		$("#resultList").html("");
-// 		var term = $("input").val();
-// 		console.log(term);
-// 		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&prop=pageimages&format=json&search=" + term + "&suggest=1&callback=?"
-// 		console.log(url);
-// 		$.getJSON(url, dataLoop);
-// 	}
-
-// 	function dataLoop(data){
-// 		console.log(data);
-// 		for(var i = 0; i < data[1].length; i++) {
-// 			$("#resultList").append("<li><a href='" + data[3][i] + "' target='_blank'>" + data[1][i] + " : " + data[2][i] + "</a></li>");
-// 		}
-// 	}
-
-// 	//event handlers
-// 	$('button').on('click', wikiSearch);
-// });
-
-function wikiSearch() {
-	$.ajax({
-        url: "http://en.wikipedia.org/w/api.php",
-        dataType: "jsonp",
-        data: {
-            'action': "opensearch",
-            'format': "json",
-            'search': $("#search").val(),
-            'prop': "pageimages"
-        },
-        success: dataLoop
-	})
-};
-
-function dataLoop(data){
-	$("#search").val("");
-	$("#resultList").html("");
-	for(var i = 0; i < data[1].length; i++) {
-		$("#resultList").append("<li><a href='" + data[3][i] + "' target='_blank'>" + data[1][i] + " : " + data[2][i] + "</a></li>");
-	}
+function autocomplete(request) {
+	url = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+request.term+'&limit=10&namespace=0&format=json&callback=?'
+	$.getJSON(url, function (data) {
+	 	$("#resultList").html("");
+	 	for(var i = 0; i < data[1].length; i++) {
+			$("#resultList").append(
+				"<a href='"+data[3][i]+"' target='_blank'><div class='searchresult'><h4>"+data[1][i]+"</h4><p>"+data[2][i]+"</p></div></a>"
+			).fadeIn("slow");
+		}	 
+    });
 }
 
-$('button').on('click', wikiSearch);
-$("input").keypress(function(event) {
-	if(event.which === 13) {
-		wikiSearch();
-	};
+$( "#search" ).autocomplete({
+	source: autocomplete,
+    autoFocus:true,
 });
