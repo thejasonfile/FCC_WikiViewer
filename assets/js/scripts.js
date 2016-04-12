5b2c33dad2547e51685a74e60d99705568075622
Jason Arnold <jason@thejasonfile.com>
2016-04-11 22:12:40 -0400

    finished styling, added fadeIn to search results

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+function autocomplete(request) {
+	url = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+request.term+'&limit=10&namespace=0&format=json&callback=?'
+	$.getJSON(url, function (data) {
+	 	$("#resultList").html("");
+	 	for(var i = 0; i < data[1].length; i++) {
+			$("#resultList").append(
+				"<a href='"+data[3][i]+"' target='_blank'><div class='searchresult fadeMe noshow'><h4>"+data[1][i]+"</h4><p>"+data[2][i]+"</p></div></a>"
+			)
+		}
+		$(".fadeMe").each(function(index){
+			$(this).delay(100*index).fadeIn(300);
+		})
+    });
+}
+
+$( "#search" ).autocomplete({
+	source: autocomplete,
+    autoFocus:true,
+});
+
